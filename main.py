0d17c1d2ab0333d37c25b12727b81541d7208003
jMortfield <j.mortfield44@gmail.com>
2025-03-13 15:19:51 -0400

    Added asterois and player shape classes

--- main.py
+++ main.py
@@ -2,11 +2,15 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
+from constants import *
+from player import Player
 
 def main():
   pygame.init()
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+  clock = pygame.time.Clock()
+  player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+  dt = 0
 
   while True:
      for event in pygame.event.get():
@@ -14,7 +18,10 @@ def main():
           return
         
      screen.fill("black")
+     player.draw(screen)
      pygame.display.flip()
+
+     dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
